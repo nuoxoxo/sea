5a0d238bb663faf1bf580a77dff24f7b5438c882
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-09-10 10:01:36 +0200

    foreach : use simple test

--- reload/ex25/ft_foreach.c
+++ reload/ex25/ft_foreach.c
@@ -6,27 +6,9 @@
 /*   By: nuxu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 10:46:48 by nuxu              #+#    #+#             */
-/*   Updated: 2021/08/12 11:43:02 by nuxu             ###   ########.fr       */
+/*   Uput_one_positive_digit_and_breakated: 2021/08/12 11:43:02 by nuxu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-
-void    ft_foreach(int *tab, int length, void(*f)(int));
-void    pnbr(int n);
-void    pn(int n);
-
-// DRIVE
-
-#include <unistd.h>
-
-int     main()
-{
-        int     arr[] = {0, -64, 512, -1024, 2147483647, -2147483648};
-        ft_foreach(arr, 6, &pnbr);
-}
-
-*/
 
 void    ft_foreach(int *tab, int length, void(*f)(int))
 {
@@ -42,33 +24,21 @@ void    ft_foreach(int *tab, int length, void(*f)(int))
 
 /*
 
-void    pn(int num)
+#include <unistd.h>
+
+void    put_one_positive_digit_and_break(int n)
 {
-        long    n;
         char    c;
-
-        n = num;
-        if      (n < 0)
-        {
-                n *= -1;
-                write(1, "-", 1);
-        }
-        if      (n < 10 && n >= 0)
-        {
-                c = n + '0';
-                write(1, &c, 1);
-        }
-        else
-        {
-                pn(n / 10);
-                c = n % 10 + '0';
-                write(1, &c, 1);
-        }
+        if (n < 10 && n >= 0)
+        c = n % 10 + '0';write(1, &c, 1);write(1, "\n", 1);
 }
 
-void    pnbr(int n)
+// DRIVE
+
+int     main()
 {
-        pn(n);
-        write(1, "\n", 1);
+        int     arr[] = {0, 9, 2, 7, 4, 6, 3, 5, 1, 8};
+        ft_foreach(arr, 10, &put_one_positive_digit_and_break);
 }
+
 */
