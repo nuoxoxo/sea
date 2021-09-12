0ff0d5f9d55523a68b2ebfc483cf1d83b68d5fbd
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-09-13 00:34:47 +0200

    c11 init : list push back

---
+++ eleven/ex00/ft_create_elem.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nuxu <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 10:46:48 by nuxu              #+#    #+#             */
+/*   Updated: 2021/08/12 11:43:02 by nuxu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    <stdlib.h>
+#include    "ft_list.h"
+
+t_list  *ft_create_elem(void *data)
+{
+        t_list  *thing;
+
+        thing = malloc(sizeof(t_list));
+        if (!thing)     return (NULL);
+        thing->data = data;
+        thing->next = NULL;
+        return (thing);
+}
+
+/*
+
+//  DRIVE
+
+#include    <stdio.h>
+
+int     main()
+{
+        char        data_1;
+        t_list      *list_1;
+        
+        int         data_2;
+        t_list      *list_2;
+        
+        data_1 = 'H';
+        list_1 = ft_create_elem(&data_1);
+        printf("thing_1->data : %c\n", *(char*)list_1->data);
+
+        data_2 = 1024;
+        list_2 = ft_create_elem(&data_2);
+        printf("thing_2->data : %d\n", *(int*)list_2->data);
+}
+
+*/
---
+++ eleven/ex01/ft_list_push_back.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nuxu <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 10:46:48 by nuxu              #+#    #+#             */
+/*   Updated: 2021/08/12 11:43:02 by nuxu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    <stdlib.h>
+#include    "ft_list.h"
+
+t_list  *ft_create_elem(void *d);
+
+void    ft_list_push_back(t_list **begin_list, void *data)
+{
+        t_list  *thing;
+
+        thing = *begin_list;
+        if (!thing)
+            *begin_list = ft_create_elem(data);
+        else
+        {
+            while (thing->next) 
+                thing = thing->next;
+            thing->next = ft_create_elem(data);
+        }
+}
+
+t_list  *ft_create_elem(void *data)
+{
+        t_list  *thing;
+
+        thing = malloc(sizeof(t_list));
+        if (!thing)
+            return (NULL);
+        thing->data = data;
+        thing->next = NULL;
+        return (thing);
+}
+
+/*
+
+// DRIVE
+
+#include    <stdlib.h>
+#include    <stdio.h>
+
+int     main(int argc, char **argv)
+{
+        (void)  argc;
+        char    data1 = *argv[1];
+        int     data2 = atoi(argv[2]);
+        t_list  *thing;
+
+        ft_list_push_back(&thing, &data1);
+        ft_list_push_back(&thing, &data2);
+        printf("%c\n%i\n", *(char *)thing->data, *(int*)thing->next->data);
+}
+
+*/
