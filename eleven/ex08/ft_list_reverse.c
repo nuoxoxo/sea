698620eefbc6a1df59fd35d04a486ea8aa709e9d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-09-15 18:12:16 +0200

    c11 : added 8. list_reverse

---
+++ eleven/ex08/ft_list_reverse.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nuxu <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 10:46:48 by nuxu              #+#    #+#             */
+/*   Updated: 2021/08/12 11:43:02 by nuxu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    <stdlib.h>
+#include    "ft_list.h"
+
+void    ft_list_reverse(t_list **begin_list)
+{
+        t_list  *self;
+        t_list  *p;
+        t_list  *n;
+
+        self = *begin_list;
+        p = NULL;
+        n = NULL;
+        while (self)
+        {
+            n = self->next;
+            self->next = p;
+            p = self;
+            self = n;
+        }
+        *begin_list = p;
+}
+
+/*
+
+// DRIVE
+
+#include    <stdio.h>
+
+t_list  *pp(int n, char **s);
+t_list  *ce(void *d);
+
+int     main(int argc, char **argv)
+{
+        t_list  *p;
+        t_list  *ptr1 = NULL;
+        t_list  *ptr2 = NULL;
+        int     i;
+        
+        p = pp(argc, argv);
+        ptr1 = p;
+        i = 0;
+
+        printf("\noriginal list \n\n");
+        while (i < argc - 1)
+        {
+            printf("[%i] : %c\n", i, *(char*)ptr1->data);
+            ptr1 = ptr1->next;
+            i++;
+        }
+        printf("\n");
+
+        ft_list_reverse(&p);
+        ptr2 = p;
+        i = 0;
+
+        printf("reversed list \n\n");
+        while (ptr2)
+        {
+            printf("[%i] : %c\n", i, *(char*)ptr2->data);
+            ptr2 = ptr2->next;
+            i++;
+        }
+        printf("\n");
+}
+
+t_list  *pp(int n, char **s)
+{
+        t_list  *head, *p;
+        int     i = 1;
+
+        if (n < 2)  return NULL;
+        head = ce(s[i++]);
+
+        p = head;
+        while (i < n)
+        {
+            p->next = ce(s[i++]);
+            p = p->next;
+        }
+        return  head;
+}
+
+t_list  *ce(void *data)
+{
+        t_list  *p;
+
+        p = malloc(sizeof(t_list));
+        if (!p) return (NULL);
+        p->data = data;
+        p->next = NULL;
+        return (p);
+}
+
+*/
